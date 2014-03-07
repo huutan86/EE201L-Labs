e64a41f44d5ec7bc515651cea1c3a307b26a2450
Colin Cammarano <colincammarano@aol.com>
2014-03-07 12:48:48 -0800

    Attempting to finish Q1

--- HW6aVerilog/largest_num_divisible_by_7_sp10/largest_num_divisible_by_7.v
+++ HW6aVerilog/largest_num_divisible_by_7_sp10/largest_num_divisible_by_7.v
@@ -32,85 +32,116 @@ D_NF = 	5'b10000 ;// "Done Not Found any non-zero number divisible by 7" state
          
 assign {Qdnf, Qdf, Qdiv, Ql, Qi} = state;
 
-always @(posedge Clk, posedge Reset) 
+always @(posedge Clk, posedge Reset) begin  : CU_n_DU
+	if (Reset) begin
+		state <= INI;
+		I <= 4'bXXXX;
+		Max <= 8'bXXXXXXXX;
+		X <= 8'bXXXXXXXX;	   // to avoid recirculating mux controlled by Reset 
+	end
+	
+	else begin
+		(* full_case, parallel_case *)
+		case (state)
+			INI	: begin
+				// state transitions in the control unit
+				if (Start)
+					state <= LD_X;
+				// RTL operations in the Data Path            	              
+				Max <= 0;
+				I <= 0;
+			end
 
-  begin  : CU_n_DU
-    if (Reset)
-       begin
-         state <= INI;
-          I <= 4'bXXXX;
-	      Max <= 8'bXXXXXXXX;
-	      X <= 8'bXXXXXXXX;	   // to avoid recirculating mux controlled by Reset 
-	    end
-    else
-       begin
-         (* full_case, parallel_case *)
-         case (state)
-	        INI	: 
-	          begin
-		         // state transitions in the control unit
-		         if (Start)
-		           state <= LD_X;
-		         // RTL operations in the Data Path            	              
-		           Max <= 0;
-		           I <= 0;
-	          end
-	        LD_X	:  // ** TODO **  complete RTL Operations and State Transitions
-	          begin
-		         // state transitions in the control unit
-
-				 
-				 
-				 
-				 
-				 
-				 
-				 
-				 
-				 
-							
-		       // RTL operations in the Data Path   
-
-			   
-			   
- 	          end
+			LD_X : begin// ** TODO **  complete RTL Operations and State Transitions
+				
+				// state transitions in the control unit
+				if(I == 4'b1111 && Max != 4'b0000 && M[I] < 7) begin
+					state <= D_F;
+				end
+				
+				else if(I == 4'b1111 && Max == 4'b0000 && M[I] < 7) begin
+					state <= D_NF;
+				end
+				
+				else if(I != 4'b1111 && M[I] >= 7) begin
+					state <= DIV;
+				end
+				
+				else begin
+					state <= LD_X;
+				end
+	
+				// RTL operations in the Data Path
+				X <= M[I];
+				
+				if(M[I] < 7) begin   
+					I = I + 1'b1;
+				end
+				  
+			end
 	        
-	        DIV :  // ** TODO **  complete RTL Operations and State Transitions
-	          begin
-	          // state transitions in the control unit
-
-			  
-			  
-			  
-			  
-			  
-			  
-			  
-			  
-			  
-	             
-	          // RTL operations in the Data Path
-
-
-
-
-			  
-	          end
+	        DIV : begin// ** TODO **  complete RTL Operations and State Transitions
+				
+				// state transitions in the control unit
+				
+				if(X <= 7) begin
+				
+					I = I + 1;
+					
+					// Max is set regardless of what happens afterwards
+					if(X == 7 && Max < M[I]) begin
+						Max <= M[I];
+					end
+					
+					// Divisible, nothing else was, last number -> done
+					if(X == 7 && I == 4'b1111 && Max == 4'b0000) begin
+						state <= D_F;
+					end
+					
+					// Not divisible, nothing else was, last number -> done, not found
+					else if(X != 7 && I == 4'b1111 && Max == 4'b0000) begin
+						state <= D_NF;
+					end
+					
+					else if(I != 4'b1111) begin
+						state <= LD_X;
+					end
+					
+					// Not divisible, something else was, last number -> done
+					else if(X != 7 && I == 4'b1111 && Max != 4'b0000) begin
+						state <= D_F;
+					end
+					
+					// Divisible, something else was, last number -> done
+					else if(X == 7 && I == 4'b1111 && Max != 4'b0000) begin
+						state <= D_F;
+					end
+					
+				end
+				
+				else begin
+					state <= DIV;
+				end
+				 
+				// RTL operations in the Data Path
+				X <= X - 7;
+				
+			end
 	        
-	        D_F	:
-	          begin  
+	        D_F	: begin  
 		         // state transitions in the control unit
 		         if (Ack)
 		           state <= INI;
 		       end    
-	        D_NF	:
-	          begin  
-		         // state transitions in the control unit
-		         if (Ack)
-		           state <= INI;
-		       end    
-      endcase
-    end 
-  end 
+	        D_NF : begin  
+				// state transitions in the control unit
+				if (Ack)
+					state <= INI;
+			end    
+			
+		endcase
+	end 
+end 
+
 endmodule  
 
