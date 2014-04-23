1a759c399d6035ff80826d303ca70728ac951c64
Colin Cammarano <colincammarano@aol.com>
2014-04-22 23:18:01 -0700

    Worked on top file more. Should have signals for CEN.

--- binary_game/game_top.v
+++ binary_game/game_top.v
@@ -75,8 +75,7 @@ module game_top
 //------------	
 // Disable the three memories so that they do not interfere with the rest of the design.
 	assign {MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS} = 5'b11111;
-	
-	
+
 //------------
 // CLOCK DIVISION
 
@@ -94,34 +93,33 @@ module game_top
 // routing resources in the FPGA.
 
 
-	assign Reset = BtnU;
+	assign Reset = BtnD;
 	
 //------------
 	// Our clock is too fast (100MHz) for SSD scanning
 	// create a series of slower "divided" clocks
 	// each successive bit is 1/2 frequency
-  always @(posedge board_clk, posedge Reset) 	
-    begin							
-        if (Reset)
-		DIV_CLK <= 0;
-        else
-		DIV_CLK <= DIV_CLK + 1'b1;
-    end
+	always @(posedge board_clk, posedge Reset) begin							
+		if (Reset)
+			DIV_CLK <= 0;
+		else
+			DIV_CLK <= DIV_CLK + 1'b1;
+	end
 //-------------------	
 	// In this design, we run the core design at full 50MHz clock!
-	assign	sys_clk = board_clk;
-	// assign	sys_clk = DIV_CLK[25];
+	assign sys_clk = board_clk;
+	// assign sys_clk = DIV_CLK[25];
 
 //------------
 // INPUT: SWITCHES & BUTTONS
 	// BtnL is used as both Start and Acknowledge. 
 	// To make this possible, we need a single clock producing  circuit.
 	
-	ee201_debouncer #(.N_dc(28)) ee201_debouncer_2 (.CLK(sys_clk), .RESET(Reset), .PB(BtnL), .DPB( ), .SCEN(BtnL_Pulse), .MCEN( ), .CCEN( ));
+	ee201_debouncer #(.N_dc(28)) ee201_debouncer_0 (.CLK(sys_clk), .RESET(Reset), .PB(BtnL), .DPB( ), .SCEN(BtnL_Pulse), .MCEN( ), .CCEN( ));
 	ee201_debouncer #(.N_dc(28)) ee201_debouncer_1 (.CLK(sys_clk), .RESET(Reset), .PB(BtnR), .DPB( ), .SCEN(BtnR_Pulse), .MCEN( ), .CCEN( ));
-	ee201_debouncer #(.N_dc(28)) ee201_debouncer_0 (.CLK(sys_clk), .RESET(Reset), .PB(BtnU), .DPB( ), .SCEN(BtnU_Pulse), .MCEN( ), .CCEN( ));
-	ee201_debouncer #(.N_dc(28)) ee201_debouncer_1 (.CLK(sys_clk), .RESET(Reset), .PB(BtnC), .DPB( ), .SCEN(BtnC_Pulse), .MCEN( ), .CCEN( ));
-	ee201_debouncer #(.N_dc(28)) ee201_debouncer_0 (.CLK(sys_clk), .RESET(Reset), .PB(BtnD), .DPB( ), .SCEN(BtnD_Pulse), .MCEN( ), .CCEN( ));
+	ee201_debouncer #(.N_dc(28)) ee201_debouncer_2 (.CLK(sys_clk), .RESET(Reset), .PB(BtnU), .DPB( ), .SCEN(BtnU_Pulse), .MCEN( ), .CCEN( ));
+	ee201_debouncer #(.N_dc(28)) ee201_debouncer_3 (.CLK(sys_clk), .RESET(Reset), .PB(BtnC), .DPB( ), .SCEN(BtnC_Pulse), .MCEN( ), .CCEN( ));
+	ee201_debouncer #(.N_dc(28)) ee201_debouncer_4 (.CLK(sys_clk), .RESET(Reset), .PB(BtnD), .DPB( ), .SCEN(BtnD_Pulse), .MCEN( ), .CCEN( ));
 	
 	// BtnR is used to generate in_AB_Pulse to record the values of 
 	// the inputs A and B as set on the switches.
@@ -132,24 +130,16 @@ module game_top
 // DESIGN
 	// On two pushes of BtnR, numbers A and B are recorded in Ain and Bin
     // (registers of the TOP) respectively
-	always @ (posedge sys_clk, posedge Reset)
-	begin
-		if(Reset)
-		 begin			// ****** TODO  in Part 2 ******
-			userInput <=  8'b00000000;  	// Is it necessary or desirable to initiate
-			// When you download the TA's .bit file or when you reset the Nexys-2 
-			// do you see "0000" on the SSDs or some random combination?
-			// When you go from DONE state to INITIAL state after working on a set
-			// Ain and Bin, do you see "0000" again in the INITIAL state?
-			// If not, what do you see?
-			
-		 end
-		else
-		 begin
-			if (q_Play || q_Practice) begin  	// Note: in_AB_Pulse is same as BtnR_Pulse.
+	always @ (posedge sys_clk, posedge Reset) begin
+		if(Reset) begin			// ****** TODO  in Part 2 ******
+			userNumber <=  8'b00000000;
+		end
+		
+		else begin
+			if (q_Play || q_Practice) begin
 				userNumber <= {Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0};
 			end
-		 end
+		end
 	end
 	
 	// the state machine module
@@ -211,44 +201,41 @@ module game_top
 	// 381.5 cycles/sec (2.62 ms per digit) [which means all 4 digits are lit once every 10.5 ms (reciprocal of 95.4 cycles/sec)] works well.
 	
 	//                  --|  |--|  |--|  |--|  |--|  |--|  |--|  |--|  |   
-    //                    |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  | 
+    //                   |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  | 
 	//  DIV_CLK[17]       |__|  |__|  |__|  |__|  |__|  |__|  |__|  |__|
 	//
 	//               -----|     |-----|     |-----|     |-----|     |
-    //                    |  0  |  1  |  0  |  1  |     |     |     |     
+    //                   |  0  |  1  |  0  |  1  |     |     |     |     
 	//  DIV_CLK[18]       |_____|     |_____|     |_____|     |_____|
 	//
 	//         -----------|           |-----------|           |
-    //                    |  0     0  |  1     1  |           |           
+    //                   |  0     0  |  1     1  |           |           
 	//  DIV_CLK[19]       |___________|           |___________|
 	//
 	
 	assign ssdscan_clk = DIV_CLK[19:18];
-	
 	assign An3	= !(~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 00
 	assign An2	= !(~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 01
 	assign An1	=  !((ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 10
 	assign An0	=  !((ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 11
 	
 	
-	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3)
-	begin : SSD_SCAN_OUT
+	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3) begin : SSD_SCAN_OUT
 		case (ssdscan_clk) 
-				  2'b00: SSD = SSD3;	// ****** TODO  in Part 2 ******
-				  2'b01: SSD = SSD2;  	// Complete the four lines
-				  2'b10: SSD = SSD1;	
-				  2'b11: SSD = SSD0;
+			2'b00: SSD = SSD3;	// ****** TODO  in Part 2 ******
+			2'b01: SSD = SSD2;  	// Complete the four lines
+			2'b10: SSD = SSD1;	
+			2'b11: SSD = SSD0;
 		endcase 
 	end
 	
 	// and finally convert SSD_num to ssd
 	// We convert the output of our 4-bit 4x1 mux
 
-	assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp} = {SSD_CATHODES};
+	assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp} = { SSD_CATHODES };
 
 	// Following is Hex-to-SSD conversion
-	always @ (SSD) 
-	begin : HEX_TO_SSD
+	always @ (SSD) begin : HEX_TO_SSD
 		case (SSD) // in this solution file the dot points are made to glow by making Dp = 0
 		    //                                                                abcdefg,Dp
 			// ****** TODO  in Part 2 ******
---
+++ binary_game/scores_ram.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:05:33 04/22/2014 
+// Design Name: 
+// Module Name:    scores_ram 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module array_Scores_RAM(clk, write_EN, input_data, read_addr, write_addr, Output);
+
+	input clk;
+	input write_EN;
+	input [13:0] input_data;
+	input [13:0] read_addr;
+	input [13:0] write_addr;
+ 
+	output [13:0] Output;
+
+	reg [13:0] array_Scores [13:0]; // Array R to store the merged array with 8 elements
+
+	always @ (posedge clk) begin  : array_Scores_RAM_logic
+		if (write_EN) begin
+			array_Scores[write_addr] <= input_data;
+		end
+	end
+ 
+	assign Output = array_Scores[read_addr];
+ 
+endmodule
